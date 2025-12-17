955a339a451248d403806352a7588ce2fc2c4f91
ShayShN <shayshn1234@gmail.com>
2025-12-17 12:54:24 +0200

    fff

---
+++ main.js
@@ -0,0 +1,7 @@
+import { getPeopleList, getCallRecord, searchByName, SearchPeopleByAge, FindDangerousPeople } from "./app.js";
+
+getPeopleList()
+getCallRecord()
+searchByName('Niro')
+SearchPeopleByAge(32)
+FindDangerousPeople()
